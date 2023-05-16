7d6ab29cd36348b8cfe7e9f4dedfad106c7627ea
TheMese <jousema.fernandez@gmail.com>
2023-05-16 10:32:06 +0200

    Added roboto font

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './frontend/App';
 import reportWebVitals from './reportWebVitals';
+import '@fontsource/roboto/500.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
